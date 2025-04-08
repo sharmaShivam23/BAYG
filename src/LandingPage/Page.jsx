033e5878e91254a8a81f387fa6ee3af61ee92fd6
Student-shivamsharma <sgivamsharma06@gmail.com>
2025-04-08 19:42:11 +0530

    code

---
+++ src/LandingPage/Page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <firstSection/>
+    </div>
+  )
+}
+
+export default Page
+
+
+const firstSection = () => {
+  return(
+    <div className='flex justify-center items-center h-screen'>
+      Hello
+    </div>
+  )
+}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='flex justify-center items-center h-screen'>
-      Hello
+      {/* Hello */}
     </div>
   )
 }
