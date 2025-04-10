3f29cfc0fc55cece5904f30b25e8b8321aa8763a
Student-shivamsharma <sgivamsharma06@gmail.com>
2025-04-10 22:52:16 +0530

    contact

--- src/pages/ContactUsPage/ContactUs.jsx
+++ src/pages/ContactUsPage/ContactUs.jsx
@@ -1,18 +1,22 @@
-import React from 'react' ;
+
+
+
+import React from 'react';
 import NavBar from '../../components/NavBar';
 import ContactCards from './ContactComponents/sectionTwo';
 import ConnectWithBAYG from './ContactComponents/sectionThree';
 import ContactForm from './ContactComponents/sectionFour';
+import ImageComp from './ContactComponents/sectionOne';
 
 const ContactUs = () => {
   return (
     <>
-    <ImageComp></ImageComp>
-    <ContactCards></ContactCards>
-    <ConnectWithBAYG></ConnectWithBAYG>
-    <ContactForm></ContactForm>
+      <ImageComp />
+      <ContactCards />
+      <ConnectWithBAYG />
+      <ContactForm />
     </>
-  )
-}
+  );
+};
 
-export default ContactUs ;
+export default ContactUs;
